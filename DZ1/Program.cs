a42ef3ea90a09d2e24b8d8f61d49f7657a0ced28
MikhailKrakhin <staisi.kramer@gmail.com>
2022-09-24 01:52:11 +0300

    DZ1.1

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine();
+﻿// /*
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// */
+
+
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа: "+stringNumber[1]);
